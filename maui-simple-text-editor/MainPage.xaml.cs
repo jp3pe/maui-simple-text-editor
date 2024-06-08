992e551947131d59fb4a15a5c2f8ac1bc9ea0f62
jp3pe <45834750+jp3pe@users.noreply.github.com>
2024-06-08 15:43:57 +0900

    파일 열기 기능 추가 (#1)
    * 주요 UI 및 기능 업데이트
    
    MainPage.xaml 및 MainPage.xaml.cs 파일에서 주요 변경사항이 있습니다.
    
    MainPage.xaml에서는 이미지 태그, 두 개의 레이블 태그, 그리고 "Click me" 버튼이 제거되었습니다. 대신 사용자로부터 텍스트 입력을 받는 새로운 에디터 태그 'TextInput'가 추가되었습니다.
    
    MainPage.xaml.cs에서는 'count' 변수와 'OnCounterClicked' 메소드가 제거되었으며, 이는 "Click me" 버튼의 클릭 횟수를 세는 기능을 제거한 것입니다. 새로운 문자열 변수 'filePath'와 'fileContent'가 추가되었으며, 이는 특정 파일의 경로와 내용을 저장하는 기능을 추가한 것입니다. 또한, 파일이 존재하는 경우 해당 파일의 내용을 'TextInput' 에디터에 표시하는 코드가 추가되었습니다. 파일이 존재하지 않는 경우에 대한 처리 코드도 추가되었지만, 현재는 아무런 동작도 수행하지 않습니다.
    
    * 파일 Open시 선택하는 UI 사용하도록 기능 추가
    
    * 파일 Open 버튼 추가 및 코드 수정

--- maui-simple-text-editor/MainPage.xaml.cs
+++ maui-simple-text-editor/MainPage.xaml.cs
@@ -2,24 +2,54 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
+            PickAndShow();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        async void PickAndShow()
         {
-            count++;
+            try
+            {
+                var result = await FilePicker.PickAsync();
+                if (result != null)
+                {
+                    var stream = await result.OpenReadAsync();
+                    using (var reader = new StreamReader(stream))
+                    {
+                        var fileContent = await reader.ReadToEndAsync();
+                        TextInput.Text = fileContent;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // 파일 선택 중에 오류가 발생한 경우에 대한 처리
+                Console.WriteLine(ex.Message);
+            }
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        private void FileOpenButton_Clicked(object sender, EventArgs e)
+        {
+            PickAndShow();
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            MyScrollView.HeightRequest = height * 0.8;
+            TextInput.HeightRequest = MyScrollView.Height * 0.8;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var mainDisplayInfo = DeviceDisplay.MainDisplayInfo;
+            var height = mainDisplayInfo.Height;
+
+            TextInput.HeightRequest = height * 0.8;
         }
     }
-
 }
