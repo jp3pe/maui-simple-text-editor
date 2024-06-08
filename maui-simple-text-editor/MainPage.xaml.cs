467c2c8c503aad438479b78f3a036ab723ea7ab3
jp3pe <rlaworhkd430430@gmail.com>
2024-06-08 15:40:07 +0900

    파일 Open 버튼 추가 및 코드 수정

--- maui-simple-text-editor/MainPage.xaml.cs
+++ maui-simple-text-editor/MainPage.xaml.cs
@@ -26,7 +26,30 @@
             catch (Exception ex)
             {
                 // 파일 선택 중에 오류가 발생한 경우에 대한 처리
+                Console.WriteLine(ex.Message);
             }
+        }
+
+        private void FileOpenButton_Clicked(object sender, EventArgs e)
+        {
+            PickAndShow();
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            MyScrollView.HeightRequest = height * 0.8;
+            TextInput.HeightRequest = MyScrollView.Height * 0.8;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var mainDisplayInfo = DeviceDisplay.MainDisplayInfo;
+            var height = mainDisplayInfo.Height;
+
+            TextInput.HeightRequest = height * 0.8;
         }
     }
 }
